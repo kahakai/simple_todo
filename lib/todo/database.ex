b07f4c83f33c431e2e01ccfe904977365fff95c6
kahakai <git@kahakai.me>
2024-04-09 22:41:14 +0100

    Add Todo.System module for supervision

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -3,8 +3,10 @@ defmodule Todo.Database do
 
   @db_folder "./persist"
 
-  def start do
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  def start_link do
+    IO.puts("Starting to-do database server.")
+
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def store(key, data) do
@@ -37,7 +39,7 @@ defmodule Todo.Database do
 
   defp start_workers do
     for index <- 1..3, into: %{} do
-      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {:ok, pid} = Todo.DatabaseWorker.start_link(@db_folder)
       {index - 1, pid}
     end
   end
