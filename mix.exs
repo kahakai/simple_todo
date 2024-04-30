405ab3124598c2032aef608fc45054ea05ce9f42
kahakai <git@kahakai.me>
2024-04-30 20:15:25 +0100

    Add dependencies for Observer support

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Todo.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :wx, :observer, :runtime_tools],
       mod: {Todo.Application, []}
     ]
   end
